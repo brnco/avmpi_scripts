b8d6f56c50a31a152cbb730e8b8ed2ba9dd67aff
Brendan Coates <brendan.emerson.coates@gmail.com>
2024-05-27 09:23:29 -0700

    updates

---
+++ avmpi_scripts/excel2airtable.py
@@ -0,0 +1,66 @@
+'''
+uploads metadata in Excel spreadsheets to Airtable
+'''
+import configparser
+import argparse
+import os
+import pathlib
+import logging
+
+
+def excel_to_airtable(kwvars):
+    '''
+    manages the upload of an Excel sheet to Airtable
+    '''
+
+
+def parse_args(args):
+    '''
+    returns dictionary of arguments parsed for our use
+    '''
+    kwvars = {}
+    if args.quiet:
+        kwvars['loglevel_print'] = logging.WARNING
+    elif args.verbose:
+        kwvars['loglevel_print'] = logging.DEBUG
+    else:
+        kwvars['loglevel_print'] = logging.INFO
+    kwvars['input'] = pathlib.Path(args.input)
+    return kwvars
+
+
+def init_args():
+    '''
+    initializes the arguments from the command line
+    '''
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('-q', '--quiet', dest='quiet',
+                        action='store_true', default=False,
+                        metavar='',
+                        help="run script in quiet mode. "
+                        "only print warnings and errors to command line")
+    parser.add_argument('-v', '--verbose', dest='verbose',
+                        action='store_true', default=False,
+                        metavar='',
+                        help="run script in verbose mode. "
+                        "print all log messages to command line")
+    parser.add_argument('-i', '--input', dest='input',
+                        metavar='',
+                        help="the input spreadsheet to upload")
+    args = parser.parse_args()
+    return args
+                            
+
+def main():
+    '''
+    do the thing
+    '''
+    print("starting...")
+    args = init_args()
+    kwvars = parse_args(args)
+    global logger
+    logger = make_log.init_log(loglevel_print=kwvars['loglevel_print'])
+    do_the_uploads
+
+if __name__ == "__main__":
+    main()
